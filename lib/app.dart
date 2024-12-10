d38ab9d4b30a80cc868b147ef9b71ad164d70858
Binu Maka <binumaka9@gmail.com>
2024-12-10 20:49:07 +0545

    changes

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:voyagevue/view/loginview.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Loginview(),
+    );
+  }
+}
