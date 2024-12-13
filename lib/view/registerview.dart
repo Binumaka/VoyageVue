45a6efba14345002b6183f4cd73b7afa35d6f78a
Binu Maka <binumaka9@gmail.com>
2024-12-13 14:36:34 +0545

    adding routes and validators

--- lib/view/registerview.dart
+++ lib/view/registerview.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:voyagevue/view/loginview.dart';
 
 class Registerview extends StatefulWidget {
   const Registerview({super.key});
 
   @override
-  State<Registerview> createState() => _RegisterviewState();
+  State<Registerview> createState() => RegisterviewState();
 }
 
-class _RegisterviewState extends State<Registerview> {
-  final _gap = const SizedBox(height: 14,);
+class RegisterviewState extends State<Registerview> {
+  final _gap = const SizedBox(
+    height: 14,
+  );
 
-  final _emailController =TextEditingController();
+  final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _usernameController = TextEditingController();
   final _confirmpasswordController = TextEditingController();
+
+  bool isPasswordVisible = false;
+  bool isConfirmPasswordVisible = false;
+
+  static String? registeredEmail;
+  static String? registeredPassword;
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +31,27 @@ class _RegisterviewState extends State<Registerview> {
         child: Column(
           children: [
             Expanded(
-              flex:1,
+              flex: 1,
               child: ClipRRect(
                 child: Container(
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(125),
-                        bottomLeft:Radius.circular(125),
-                      ),
-                      color: Color.fromARGB(199, 181, 181, 183),
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(125),
+                      bottomLeft: Radius.circular(125),
                     ),
-                    child: Center(
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 200,
-                        child: Image.asset(
-                          'assets/images/image1.png',
-                          fit: BoxFit.contain,
-                          ),
+                    color: Color.fromARGB(199, 181, 181, 183),
+                  ),
+                  child: Center(
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 200,
+                      child: Image.asset(
+                        'assets/images/image1.png',
+                        fit: BoxFit.contain,
                       ),
                     ),
+                  ),
                 ),
               ),
             ),
@@ -51,7 +60,8 @@ class _RegisterviewState extends State<Registerview> {
             ),
             Expanded(
               flex: 2,
-              child: Container(
+              child: SingleChildScrollView(
+                child: Container(
                   width: double.infinity,
                   decoration: const BoxDecoration(
                     borderRadius: BorderRadius.only(
@@ -66,25 +76,25 @@ class _RegisterviewState extends State<Registerview> {
                       children: [
                         const Text(
                           'Register',
-                          style:TextStyle(
-                              fontSize: 38,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                            ),
+                          style: TextStyle(
+                            fontSize: 38,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
                           ),
-                          _gap,
+                        ),
+                        _gap,
                         TextFormField(
                           controller: _emailController,
                           decoration: InputDecoration(
                             labelText: 'Email',
                             hintText: 'abc@gmail.com',
                             hintStyle: const TextStyle(
-                              color:Color.fromARGB(255, 162, 158, 158),
+                              color: Color.fromARGB(255, 162, 158, 158),
                             ),
-                            border:OutlineInputBorder(
+                            border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(20),
-                            ), 
                             ),
+                          ),
                         ),
                         _gap,
                         TextFormField(
@@ -93,40 +103,42 @@ class _RegisterviewState extends State<Registerview> {
                             labelText: 'Username',
                             hintText: 'abc',
                             hintStyle: const TextStyle(
-                              color:Color.fromARGB(255, 162, 158, 158),
+                              color: Color.fromARGB(255, 162, 158, 158),
                             ),
-                            border:OutlineInputBorder(
+                            border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(20),
-                            ), 
                             ),
+                          ),
                         ),
                         _gap,
                         TextFormField(
                           controller: _passwordController,
+                          obscureText: !isPasswordVisible,
                           decoration: InputDecoration(
                             labelText: 'Password',
                             hintText: '********',
                             hintStyle: const TextStyle(
-                              color:Color.fromARGB(255, 162, 158, 158),
+                              color: Color.fromARGB(255, 162, 158, 158),
                             ),
-                            border:OutlineInputBorder(
+                            border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(20),
-                            ), 
                             ),
+                          ),
                         ),
                         _gap,
                         TextFormField(
                           controller: _confirmpasswordController,
+                          obscureText: !isConfirmPasswordVisible,
                           decoration: InputDecoration(
                             labelText: 'Confirm Password',
                             hintText: '********',
                             hintStyle: const TextStyle(
-                              color:Color.fromARGB(255, 162, 158, 158),
+                              color: Color.fromARGB(255, 162, 158, 158),
                             ),
-                            border:OutlineInputBorder(
+                            border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(20),
-                            ), 
                             ),
+                          ),
                         ),
                         const SizedBox(
                           height: 20,
@@ -135,41 +147,73 @@ class _RegisterviewState extends State<Registerview> {
                           width: double.infinity,
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              backgroundColor: Colors.blue,
+                              padding: const EdgeInsets.symmetric(vertical: 6),
                             ),
-                            
-                            backgroundColor: Colors.blue,
-                            padding: const EdgeInsets.symmetric(vertical: 6),
-                          ),
-                            onPressed: () {}, 
-                            child: Text(
-                              'Sign In',
+                            onPressed: () {
+                              if (_passwordController.text ==
+                                  _confirmpasswordController.text) {
+                                // Store the email and password in memory
+                                setState(() {
+                                  registeredEmail = _emailController.text;
+                                  registeredPassword = _passwordController.text;
+                                });
+                
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Registered successfully!')),
+                                );
+                
+                                // Navigate back to LoginPageView
+                                Future.delayed(const Duration(seconds: 1), () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const Loginview(),
+                                    ),
+                                  );
+                                });
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Passwords do not match!')),
+                                );
+                              }
+                            },
+                            child: const Text(
+                              'Sign Up',
                               style: TextStyle(
                                 fontSize: 30,
                                 fontWeight: FontWeight.w300,
                                 color: Colors.white,
                               ),
-                              ),
                             ),
+                          ),
                         ),
                         _gap,
                         Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Already have an account? ",
-                                style: TextStyle(color: Colors.grey.shade800),
-                              ),
-                              const Text(
-                                "Sign In",
-                                style: TextStyle(color: Colors.blue),
-                              )
-                            ],
-                          ),
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Already have an account?'),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Loginview()),
+                                );
+                              },
+                              child: const Text('Sign In'),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
+                ),
               ),
             ),
           ],
