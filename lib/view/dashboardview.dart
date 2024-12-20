315ad1a42ec742bee77e9d61212dc8b5a5c9f250
Binu Maka <binumaka9@gmail.com>
2024-12-20 18:13:23 +0545

    Dashboard screen

--- lib/view/dashboardview.dart
+++ lib/view/dashboardview.dart
@@ -1,21 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:voyagevue/view/bookinglistview.dart';
+import 'package:voyagevue/view/bucketlistview.dart';
+import 'package:voyagevue/view/homeview.dart';
+import 'package:voyagevue/view/profileview.dart';
+import 'package:voyagevue/view/schedule.dart';
 
-class Dashboardview extends StatelessWidget {
+class Dashboardview extends StatefulWidget {
   const Dashboardview({super.key});
 
+  @override
+  State<Dashboardview> createState() => _DashboardviewState();
+}
+
+class _DashboardviewState extends State<Dashboardview> {
+  int _selectIndex = 0;
+  List<Widget> lstBottomScreen = [
+    const Homeview(),
+    const Bookinglistview(),
+    const Schedule(),
+    const Bucketlistview(),
+    const Profileview(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 199, 179, 234),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pushNamed(context, '/login');
-          },
+        backgroundColor: const Color(0xffC8C5C5),
+        title: Row(
+          children: [
+            Image.asset(
+              "assets/images/imgvoyagevue.png",
+              height: 250,
+              width: 360,
+            )
+          ],
         ),
+        centerTitle: true,
+      ),
+      body: lstBottomScreen[_selectIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 22,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.book_outlined,
+              size: 22,
+            ),
+            label: 'Bookings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.calendar_today,
+              size: 22,
+            ),
+            label: 'Schedule',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+                size: 22,
+              ),
+              label: 'Bucket-List'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+                size: 22,
+              ),
+              label: 'Profile'),
+        ],
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: const Color.fromARGB(255, 57, 57, 57),
+        currentIndex: _selectIndex,
+        onTap: (index) {
+          setState(() {
+            _selectIndex = index;
+          });
+        },
       ),
     );
   }
