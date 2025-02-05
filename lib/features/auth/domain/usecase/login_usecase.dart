db2dde114d2ff2b32806b50c3181a085bdca76ed
Binu Maka <binumaka9@gmail.com>
2025-02-05 14:50:39 +0545

    adding shared preferences

--- lib/features/auth/domain/usecase/login_usecase.dart
+++ lib/features/auth/domain/usecase/login_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:voyagevue/app/shared_prefs/token_shared_prefs.dart';
 import 'package:voyagevue/app/usecase/usecase.dart';
 import 'package:voyagevue/core/error/failure.dart';
 import 'package:voyagevue/features/auth/domain/repository/auth_repository.dart';
@@ -24,12 +25,26 @@ class LoginParams extends Equatable {
 
 class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
   final IAuthRepository repository;
+  final TokenSharedPrefs tokenSharedPrefs;
 
-  LoginUseCase(this.repository);
+  LoginUseCase(this.repository, this.tokenSharedPrefs);
 
   @override
   Future<Either<Failure, String>> call(LoginParams params) {
     // IF api then store token in shared preferences
-    return repository.loginUser(params.email, params.password);
+    return repository
+    .loginUser(params.email, params.password)
+    .then((value) {
+      return value.fold(
+        (failure) => Left(failure),
+        (token) {
+          tokenSharedPrefs.saveToken(token);
+          tokenSharedPrefs.getToken().then((value) {
+            print(value);
+          });
+          return Right(token);
+        }
+      );
+    });
   }
 }
