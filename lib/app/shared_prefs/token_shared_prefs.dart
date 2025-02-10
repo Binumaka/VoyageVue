5622ca431f8e5ba55967738022421f5f771aa64e
Binu Maka <binumaka9@gmail.com>
2025-02-10 16:36:52 +0545

    changes in shared pref

--- lib/app/shared_prefs/token_shared_prefs.dart
+++ lib/app/shared_prefs/token_shared_prefs.dart
@@ -24,4 +24,35 @@ class TokenSharedPrefs {
       return Left(SharedPrefsFailure(message: e.toString()));
     }
   }
+  
+  // Remove token
+  Future<void> removeToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+  }
+
+  // Save user ID
+  Future<void> saveUserId(String userId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('userId', userId);
+  }
+
+  // Get user ID
+  Future<String?> getUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userId');
+  }
+
+  // Remove user ID
+  Future<void> removeUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('userId');
+  }
+
+  // Remove both user ID and token
+  Future<void> clearUserData() async {
+    await _sharedPreferences.remove('token');
+    await _sharedPreferences.remove('userId');
+    await _sharedPreferences.remove('username');
+  }
 }
