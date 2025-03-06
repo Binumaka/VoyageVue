9594080967284984a68b19d953cd51454297bd32
Binu Maka <binumaka9@gmail.com>
2025-03-06 21:01:09 +0545

    desitnation model

---
+++ lib/features/destination/data/model/destination_api_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'destination_api_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+
+Map<String, dynamic> _$DestinationApiModelToJson(
+        DestinationApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'title': instance.title,
+      'category': instance.category,
+      'image': instance.image,
+      'image1': instance.image1,
+      'image2': instance.image2,
+      'bestTimeToVisit': instance.bestTimeToVisit,
+      'location': instance.location,
+      'description': instance.description,
+      'section': instance.section,
+    };
